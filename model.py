3f482c5c8417d601c6cc6d58fe764ed41015e5a5
ingo.steldermann <steldermann@mbd.rwth-aachen.de>
2026-04-13 21:57:50 +0200

    Working SimulationSection + dynamic model cards

--- model.py
+++ model.py
@@ -2,40 +2,63 @@ import param
 import panel as pn
 
 from basicelements import Card, Section
-from docstring_crawler import get_class_docstring
-from zoomy_core.model.models.shallow_moments import ShallowMoments
 
 pn.extension('katex')
 
+# Model descriptions for the card view
+_MODELS = [
+    {
+        "title": "Scalar Advection",
+        "latex": r"$\partial_t u + a \, \partial_x u = 0$",
+        "desc": "Linear advection of a scalar field at constant speed.",
+    },
+    {
+        "title": "SWE (SME L0)",
+        "latex": r"$\partial_t \begin{pmatrix} h \\ hu \end{pmatrix} + \partial_x \begin{pmatrix} hu \\ hu^2/h + gh^2/2 \end{pmatrix} = 0$",
+        "desc": "Shallow water equations (depth-averaged, inviscid).",
+    },
+    {
+        "title": "SME L1",
+        "latex": r"$\partial_t Q + \nabla \cdot F(Q) + NC(Q) : \nabla Q = S(Q)$",
+        "desc": "Shallow Moment Equations level 1 (vertical velocity profile).",
+    },
+    {
+        "title": "Advection-Diffusion",
+        "latex": r"$\partial_t u + a \, \partial_x u = \nu \, \partial_{xx} u$",
+        "desc": "Scalar advection with diffusion (IMEX time integration).",
+    },
+]
+
+
 class ModelCard(Card):
 
-    def __init__(self, parent_app, latex_description, model_class, **params):
+    def __init__(self, parent_app, latex_description, description="", **params):
         super().__init__(parent_app, **params)
-        self._code=model_class
-        self._doc= get_class_docstring(self._code)
-
         latex = pn.pane.LaTeX(latex_description, width=300)
-
-        self._layout = pn.Column(self.title, latex, self._btn, styles=self._default_style)
-
+        desc = pn.pane.Markdown(description, width=300)
+        self._layout = pn.Column(self.title, latex, desc, self._btn,
+                                  styles=self._default_style)
 
     def get_controls(self):
-        """Optional: Return extra widgets unique to this card."""
-        return [pn.widgets.StaticText(value="No extra controls for this card.")]
+        return [pn.widgets.StaticText(value="Select a model, then configure in Simulation tab.")]
+
 
 class ModelSection(Section):
 
     def __init__(self, parent_app, **params):
         super().__init__(**params)
+        self.title = 'Model'
 
-        self.title='Model'
+        cards = []
+        for m in _MODELS:
+            card = ModelCard(
+                parent_app,
+                latex_description=m["latex"],
+                description=m.get("desc", ""),
+                title=m["title"],
+            )
+            self.manager.add_card(card)
+            cards.append(card)
 
-        description =  r'$\partial_t {Q} + \nabla \cdot {F}({Q}) + {NC}({Q}) : \nabla {Q} = {S}({Q})$'
-        model1 = ModelCard(parent_app, latex_description=description, model_class=ShallowMoments, title="Model #1")
-        model2 = ModelCard(parent_app, latex_description=description, model_class=ShallowMoments, title="Model #2")
-
-        self.manager.add_card(model1)
-        self.manager.add_card(model2)
-
-        self.manager.selected_card = model1
-
+        if cards:
+            self.manager.selected_card = cards[0]
